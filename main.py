c766359b333ec236b3a37552affcca3a7d0afb9e
Zach Brock <zbrock.eng@gmail.com>
2022-02-04 10:15:18 -0600

    more problems

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
 class Solution(object):
     
-    
+    # Given an integer, convert to roman numerals
     def romanToInt(self, s):
         """
         :type s: str
@@ -28,9 +33,74 @@ class Solution(object):
         return sum
 
 
+    # Given two linked lists representing two nums in reverse order
+    # add the two numbers together
+    # Return as a similar reversed linked list
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        result = ListNode(0)
+        resultTail = result
+        carry = 0
+        
+        # while either list contains value or there is a carry value
+        while l1 or l2 or carry:
+            val1 = (l1.val if l1 else 0)
+            val2 = (l2.val if l2 else 0)
+            # divmod returns the whole num quotient and remainder 
+            carry, out = divmod(val1 + val2 + carry, 10)
+            
+            # set next list node for result
+            resultTail.next = ListNode(out)
+            resultTail = resultTail.next
+            
+            # go to next list node if exists
+            l1 = (l1.next if l1 else None)
+            l2 = (l2.next if l2 else None)
+        
+        return result.next
+        
+    # multiply two strings
+    def multiply(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        return (str(int(num1) * int(num2)))
+
+    # Determine the length of the longest substring
+    # without repeating characters
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if not s: return 0
+        elif len(s) == 1: return 1
+
+        used = {}
+
+        maxl = start = 0
+
+        for index , char in enumerate(s):
+            if char in used and start <= used[char]:
+                start = used[char] + 1
+            else:
+                maxl = max(maxl, index - start + 1)
+
+            used[char] = index
+        return maxl
+                
+
 def main():
     solution = Solution()
-    num = solution.romanToInt('MCMXCIV')
-    print(num)
+
+    ans = solution.lengthOfLongestSubstring("abccq")
+    print(ans)
+
 
 if __name__ == "__main__": main()
